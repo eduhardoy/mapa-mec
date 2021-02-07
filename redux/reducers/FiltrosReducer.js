a991ed30e8fb33610a61b49a69d1734d29e08c37
Gonzalo iniguez <gonza.ini94@gmail.com>
2021-02-07 20:45:28 -0300

    Filtros Reducer in progress

---
+++ redux/reducers/FiltrosReducer.js
@@ -0,0 +1,31 @@
+import * as type from "../types";
+
+const initialState = {
+  departamentos:[],
+  localidades:[],
+  dependencia:[],
+  estado:[],
+  jurisdiccion:[],
+  organismoDependencia:[],
+  nivel:[],
+  modalidad:[],
+  gestion:[],
+  ambito:[],
+  aulaDigitalMovil:[],
+  internet:[],
+  aguaPotable:[],
+  proveedoresInternet:[],
+  agrupaciones:[]
+
+};
+
+const filtrosReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case type.SET_DEPARTAMENTOS:
+      return { ...state, departamentos: action.payload };
+    default:
+      return state;
+  }
+};
+
+export default filtrosReducer;
