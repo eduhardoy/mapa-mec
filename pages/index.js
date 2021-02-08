79db29f1e423fa319e769ccc4f565bcf8fd85e32
Gonzalo iniguez <gonza.ini94@gmail.com>
2021-02-07 21:05:53 -0300

    localidades y departamentos

--- pages/index.js
+++ pages/index.js
@@ -11,22 +11,43 @@ import { wrapper } from "../redux/store";
 
 export const getServerSideProps = wrapper.getServerSideProps(
   async ({ store }) => {
+    //LOCALIZACIONES
     const localizaciones = await axios.get(
       "http://200.10.111.88:1337/localizaciones"
     );
-    const images = await axios.get("http://200.10.111.88:1337/imagenes");
     store.dispatch({
       type: type.LOAD_LOCALIZACIONES,
       payload: localizaciones.data,
     });
+
+    //IMAGENES
+    const images = await axios.get("http://200.10.111.88:1337/imagenes");
     store.dispatch({ type: type.LOAD_IMAGES, payload: images.data });
+
+    //DEPARTAMENTOS
+    const departamentos = await axios.get(
+      "http://200.10.111.88:1337/departamentos"
+    );
+    store.dispatch({
+      type: type.LOAD_DEPARTAMENTOS,
+      payload: departamentos.data,
+    });
+
+    //LOCALIDADES 
+    const localidades = await axios.get(
+      "http://200.10.111.88:1337/localidades"
+    );
+    store.dispatch({
+      type: type.LOAD_LOCALIDADES,
+      payload: localidades.data,
+    });
+
     return { props: {} };
   }
 );
 
 // markup
 const IndexPage = () => {
-
   return (
     <Layout>
       <div>
--- redux/reducers/index.js
+++ redux/reducers/index.js
@@ -1,11 +1,15 @@
 import { combineReducers } from "redux";
 import barReducer from "./BarReducer";
+import departamentosReducer from "./DepartamentosReducer";
 import imagesReducer from "./ImagesReducer";
+import localidadesReducer from "./LocalidadesReducer";
 import localizacionesReducer from "./LocalizacionesReducer";
 import marcadoresReducer from "./MarcadoresReducer";
 
 const rootReducer = combineReducers({
   localizacion: localizacionesReducer,
+  departamento: departamentosReducer,
+  localidad: localidadesReducer,
   marcador: marcadoresReducer,
   imagen: imagesReducer,
   bar: barReducer,
