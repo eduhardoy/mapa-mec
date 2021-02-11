07190a2f5f23dd9b19321d8b33c9c095fca597e4
Gonzalo iniguez <gonza.ini94@gmail.com>
2021-02-11 20:54:49 -0300

    Localidades Filter rdy

--- components/FilterListener/index.js
+++ components/FilterListener/index.js
@@ -3,7 +3,7 @@ import { connect } from "react-redux";
 import * as type from "../../redux/types";
 import { updateMarcadores } from "../../redux/actions/MarcadoresActions";
 
-function FilterListener({ localizaciones, filtros, updateMarcadores, localidadesBase, setLocalidadesFiltered }) {
+function FilterListener({ localizaciones, filtros, updateMarcadores, localidadesBase, setLocalidadesFiltered, setLocalidadesFilter }) {
   const [filtered, setFiltered] = React.useState([]);
   //desctructuring
   const { departamentos, localidades } = filtros;
@@ -11,33 +11,40 @@ function FilterListener({ localizaciones, filtros, updateMarcadores, localidades
   //FILTRAR POR DEPARTAMENTO
   React.useEffect(async () => {
     let localidadesFiltered = []
+    let localidadesFilter = []
     const filterByDepartamentos = localizaciones.filter((loc) => {
       if (loc.domicilio) {
         if (departamentos.includes(loc.domicilio.departamento.id)) {
           let locFiltered = localidadesBase.filter(localidad => localidad.departamento.id == loc.domicilio.departamento.id)
           locFiltered.map(locFil => {
-            if (localidadesFiltered.indexOf(locFil) === -1)
+            if (localidadesFiltered.indexOf(locFil) === -1) {
               localidadesFiltered.push(locFil)
+              localidadesFilter.push(locFil.id)
+            }
+
           })
           return true;
         }
       }
       return false;
     });
+    setLocalidadesFilter(localidadesFilter)
     setLocalidadesFiltered(localidadesFiltered)
-    setFiltered(filterByDepartamentos);
-  }, [departamentos]);
+  }, [departamentos,]);
 
   //FILTAR POR LOCALIDADES (DEPENDE DE DEPARTAMENTOS)
   React.useEffect(async () => {
-    const filter = filtered.filter(loc => {
-      if (localidades.includes(loc.domicilio.localidad.id)) {
-        return true
+    const filter = localizaciones.filter(loc => {
+      if (loc.domicilio && loc.domicilio.localidad) {
+        if (localidades.includes(loc.domicilio.localidad.id)) {
+          return true
+        }
       }
+      return false
     })
-
+    console.log(filtered)
     setFiltered(filter)
-  },[localidades])
+  }, [localidades])
 
   //FINAL
   React.useEffect(() => {
@@ -55,7 +62,8 @@ const mapStateToProps = (state) => ({
 
 const mapDispatchToProps = (dispatch) => ({
   updateMarcadores: (arg) => dispatch(updateMarcadores(arg)),
-  setLocalidadesFiltered: (arg) => dispatch({ type: type.PUT_LOCALIDADES_FILTERED, payload: arg })
+  setLocalidadesFiltered: (arg) => dispatch({ type: type.PUT_LOCALIDADES_FILTERED, payload: arg }),
+  setLocalidadesFilter: (arg) => dispatch({ type: type.SET_LOCALIDADES, payload: arg })
 });
 
 export default connect(mapStateToProps, mapDispatchToProps)(FilterListener);
