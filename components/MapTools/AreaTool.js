37a24327bc40ba9128da0caeb60f2769b213b627
Edu Hardoy <eduhardoy@gmail.com>
2021-03-12 18:17:15 -0300

    Tools start

---
+++ components/MapTools/AreaTool.js
@@ -0,0 +1,10 @@
+import React from "react";
+import styled from "styled-components";
+
+const AreaWrapper = styled.div``;
+
+const AreaTool = () => {
+  return <AreaWrapper></AreaWrapper>;
+};
+
+export default AreaTool;
---
+++ components/MapTools/DistanceTool.js
@@ -0,0 +1,10 @@
+import React from "react";
+import styled from "styled-components";
+
+const DistanceWrapper = styled.div``;
+
+const DistanceTool = () => {
+  return <DistanceWrapper></DistanceWrapper>;
+};
+
+export default DistanceTool;
